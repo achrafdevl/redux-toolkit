4fa18419783c7f19885ecfd05a086201a8a9d251
achraf <achrafchair6@gmail.com>
2025-01-23 01:24:55 +0100

    add form component

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import habitsReducer from "./habit-slice"
 
-export const store = configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    habits: habitsReducer,
+  },
 })
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AddDispatch = typeof store.dispatch;
+export default store;
